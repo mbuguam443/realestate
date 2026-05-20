af433695ad9daedb4c5b7d82f8f0564131418401
Mathew Mbugua <mbuguam443@gmail.com>
2026-05-20 19:32:07 +0300

    added staticfile setting

--- real_estate/settings.py
+++ real_estate/settings.py
@@ -122,7 +122,9 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
-STATICFILES_DIRS = [ BASE_DIR / 'static',]
+STATICFILES_DIRS = [ BASE_DIR / 'static']
+STATIC_ROOT = BASE_DIR / 'staticfiles'
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
