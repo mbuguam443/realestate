e0e9a0f0613ff772c98fe1d4736564cd2f8dde34
Mathew Mbugua <mbuguam443@gmail.com>
2026-05-21 03:45:14 +0300

    Use simpler WhiteNoise static storage

--- real_estate/settings.py
+++ real_estate/settings.py
@@ -128,7 +128,7 @@ STATIC_URL = 'static/'
 
 STATICFILES_DIRS = [ BASE_DIR / 'static']
 STATIC_ROOT = BASE_DIR / 'staticfiles'
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedStaticFilesStorage'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
